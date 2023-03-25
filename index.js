4185cd04ec173ab9443fe7c2ffe2169e7546c854
Farid Amimri <farid.amimri@gmail.com>
2023-03-25 18:06:34 +0000

    Set Nodejs MongoDB Connection

--- index.js
+++ index.js
@@ -2,7 +2,24 @@
 
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const userRoute = require('./routes/user')
+const authRoute = require('./routes/auth')
 
-app.listen(5000, () => {
-  console.log('Backend server is running !')
+dotenv.config()
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log('MongoDB connected successfully'))
+  .catch((err) => {
+    log.error(err)
+  })
+
+app.use(express.json())
+app.use('/api/auth', authRoute)
+app.use('/api/users', userRoute)
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log('Backend server is running')
 })
