e1ecd2865ef7549b0ae66fc24e71c3f763e63d85
Farid Amimri <farid.amimri@gmail.com>
2023-03-26 09:49:15 +0000

    Implement Nodejs Jsonwebtoken

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@
 const router = require('express').Router()
 const User = require('../models/User')
 const CryptoJS = require('crypto-js')
+const jwt = require('jsonwebtoken')
 
 // REGISTER
 router.post('/register', async (req, res) => {
@@ -34,12 +35,22 @@ router.post('/login', async (req, res) => {
       process.env.CRYPTO_KEY
     )
     const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8)
+
     originalPassword !== req.body.password &&
       res.status(401).json('wrong credentials !')
 
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin
+      },
+      process.env.JWT_KEY,
+      { expiresIn: '3d' }
+    )
+
     const { password, ...others } = user._doc // Destructure user info and send all infos exept password
 
-    res.status(201).json(others)
+    res.status(200).json({ ...others, accessToken })
   } catch (error) {
     res.status(500).json(error)
   }
