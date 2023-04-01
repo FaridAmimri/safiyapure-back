82b83342c072a0a58ad826f5a5aafcb3752e08e4
Farid Amimri <farid.amimri@gmail.com>
2023-04-01 18:41:32 +0200

    NodeJs Get Operation for All Users route with JWT

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,11 @@
 
 const router = require('express').Router()
 const User = require('../models/User')
-const { verifyToken, verifyTokenAndAuthorization } = require('./verifyToken')
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin
+} = require('./verifyToken')
 
 // UPDATE
 router.put('/:id', verifyTokenAndAuthorization, async (req, res) => {
@@ -28,7 +32,7 @@ router.put('/:id', verifyTokenAndAuthorization, async (req, res) => {
 })
 
 // DELETE
-router.delete('/:id', verifyTokenAndAuthorization, async (req, res) => {
+router.delete('/:id', verifyTokenAndAdmin, async (req, res) => {
   try {
     await User.findByIdAndDelete(req.params.id)
     res.status(200).json('User has been deleted successfully')
@@ -38,13 +42,24 @@ router.delete('/:id', verifyTokenAndAuthorization, async (req, res) => {
 })
 
 // GET ONE USER
-router.get('/:id', verifyTokenAndAuthorization, async (req, res) => {
+router.get('/:id', verifyTokenAndAdmin, async (req, res) => {
   try {
-    await User.findByIdAndDelete(req.params.id)
-    res.status(200).json('User has been deleted successfully')
+    const user = await User.findById(req.params.id)
+    const { password, ...others } = user._doc
+
+    res.status(200).json(others)
   } catch (error) {
     res.status(500).json(error)
   }
 })
 
+// GET ALL USERS
+router.get('/', verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const users = await User.find()
+    res.status(200).json(users)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
 module.exports = router
