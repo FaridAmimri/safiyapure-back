3702626ebdee7fd51ea959c15e52948936218b89
Farid Amimri <farid.amimri@gmail.com>
2023-04-01 19:03:46 +0200

    Add query to get only 5 latest users

--- routes/user.js
+++ routes/user.js
@@ -55,8 +55,11 @@ router.get('/:id', verifyTokenAndAdmin, async (req, res) => {
 
 // GET ALL USERS
 router.get('/', verifyTokenAndAdmin, async (req, res) => {
+  const query = req.query.new
   try {
-    const users = await User.find()
+    const users = query
+      ? await User.find().sort({ id: -1 }).limit(5) // if query true return latest 5 users
+      : await User.find()
     res.status(200).json(users)
   } catch (error) {
     res.status(500).json(error)
