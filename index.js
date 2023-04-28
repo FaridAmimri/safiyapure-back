7190b195b632b07e5071ce103cfe51561744817d
Farid Amimri <farid.amimri@gmail.com>
2023-04-28 16:11:27 +0200

    Fix order route mistake

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const dotenv = require('dotenv')
 const authRoute = require('./routes/auth')
 const userRoute = require('./routes/user')
 const productRoute = require('./routes/product')
-const cartRoute = require('./routes/product')
-const cartOrder = require('./routes/order')
+const cartRoute = require('./routes/cart')
+const orderRoute = require('./routes/order')
+const stripeRoute = require('./routes/stripe')
+const cors = require('cors')
 
 dotenv.config()
 
@@ -19,12 +21,14 @@ mongoose
     log.error(err)
   })
 
+app.use(cors())
 app.use(express.json())
 app.use('/api/auth', authRoute)
 app.use('/api/users', userRoute)
 app.use('/api/products', productRoute)
 app.use('/api/carts', cartRoute)
-app.use('/api/orders', cartOrder)
+app.use('/api/orders', orderRoute)
+app.use('/api/checkout', stripeRoute)
 
 app.listen(process.env.PORT || 5000, () => {
   console.log('Backend server is running')
