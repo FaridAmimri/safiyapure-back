4d74a7754592fccedabf08f6e2409d953d7b1e36
Farid Amimri <farid.amimri@gmail.com>
2023-05-04 12:47:31 +0200

    Clean code

--- routes/passwordReset.js
+++ routes/passwordReset.js
@@ -6,7 +6,7 @@ const CryptoJS = require('crypto-js')
 const jwt = require('jsonwebtoken')
 const nodemailer = require('nodemailer')
 
-// Send Email link
+// Send password reset link
 router.post('/reset', async (req, res) => {
   const { email } = req.body
   try {
@@ -14,10 +14,13 @@ router.post('/reset', async (req, res) => {
     !oldUser && res.status(401).json("Ce compte email n'existe pas")
     res.status(200).json(oldUser)
 
-    const secret = process.env.JWT_KEY + oldUser.password
-    const token = jwt.sign({ email: oldUser.email, id: oldUser._id }, secret, {
-      expiresIn: '5m'
-    })
+    const token = jwt.sign(
+      { email: oldUser.email, id: oldUser._id },
+      process.env.JWT_KEY,
+      {
+        expiresIn: '5m'
+      }
+    )
     const link = `http://localhost:5000/api/password/reset/${oldUser._id}/${token}` // When user clicks on forgot password, he will get this link back
 
     var transporter = nodemailer.createTransport({
@@ -45,37 +48,37 @@ router.post('/reset', async (req, res) => {
   } catch (error) {}
 })
 
-// Get reset form password
+// Check if user exists in database
 router.get('/reset/:id/:token', async (req, res) => {
   const { id, token } = req.params
   console.log(req.params)
 
   const oldUser = await User.findOne({ _id: id })
   !oldUser && res.status(401).json('Utilisateur inexistant')
-  const secret = process.env.JWT_KEY + oldUser.password
 
   try {
-    const verify = jwt.verify(token, secret) // verify if the secret belongs to the user
+    const verify = jwt.verify(token, process.env.JWT_KEY) // verify if the token belongs to the user
     res.render('index', { email: verify.email, status: 'Not verified' })
   } catch (error) {
     res.send('Not verified') // if token does not match
   }
 })
 
-// Reset password
+// Reset user password
 router.post('/reset/:id/:token', async (req, res) => {
   const { id, token } = req.params
 
   const oldUser = await User.findOne({ _id: id })
   !oldUser && res.status(401).json('Utilisateur inexistant')
-  const secret = process.env.JWT_KEY + oldUser.password
 
   try {
-    const verify = jwt.verify(token, secret)
+    const verify = jwt.verify(token, process.env.JWT_KEY)
+
     const encryptedPassword = CryptoJS.AES.encrypt(
       req.body.password,
       process.env.CRYPTO_KEY
     ).toString()
+
     await User.findByIdAndUpdate(
       {
         _id: id
@@ -86,9 +89,11 @@ router.post('/reset/:id/:token', async (req, res) => {
         }
       }
     )
-    res.status(201).json('Mot de passe modifié avec succès')
-
-    res.render('index', { email: verify.email, status: 'verified' })
+    res
+      .status(201)
+      .json(
+        'Mot de passe modifié avec succès. Merci de retourner sur la page Login'
+      )
   } catch (error) {
     res.status(401).json("Une erreur s'est produite")
   }
